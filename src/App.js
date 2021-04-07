35657c9b1e4e1fd4bc25c3ef0374b5dd59737634
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-04-07 22:19:42 +0700

    Using ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main:"#2e1667",
+      },
+      secondary: {
+        main:"#c7d8ed",
+      },
+    },
+    typography: {
+      fontFamily: [
+        'Roboto'
+      ],
+      h4: {
+        fontWeight: 600,
+        fontSize: 28,
+        lineHeight: '2rem',
+        },
+      h5: {
+        fontWeight: 100,
+        lineHeight: '2rem',
+      },
+    },
+  });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                
+            </ThemeProvider>
+        </div>
+    )
 }
 
-export default App;
+export default App
